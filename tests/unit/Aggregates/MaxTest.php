09a115f131a402e51bad7c1541d1910ad80c06ca
Luke Roantree <lroantree@gcdtech.com>
2016-08-10 10:21:05 +0100

    Added Unit tests for Min and Max classes for Join feature

---
+++ tests/unit/Aggregates/MaxTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luke
+ * Date: 10/08/2016
+ * Time: 09:35
+ */
+
+namespace Rhubarb\Stem\Tests\unit\Aggregates;
+
+
+class MaxTest
+{
+
+}
---
+++ tests/unit/Repositories/MySql/Aggregates/MySqlMaxTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luke
+ * Date: 10/08/2016
+ * Time: 09:40
+ */
+
+namespace Rhubarb\Stem\Tests\unit\Repositories\MySql\Aggregates;
+
+
+class MySqlMaxTest
+{
+
+}
