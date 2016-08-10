fcf8609331c88ad5cb3b7ad50754d0ec3cfe5c89
Luke Roantree <lroantree@gcdtech.com>
2016-08-10 10:24:01 +0100

    Added Unit tests for Min and Max on Join feature

--- tests/unit/Aggregates/MaxTest.php
+++ tests/unit/Aggregates/MaxTest.php
@@ -1,15 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Luke
- * Date: 10/08/2016
- * Time: 09:35
- */
-
 namespace Rhubarb\Stem\Tests\unit\Aggregates;
 
-
-class MaxTest
+use Rhubarb\Crown\Tests\Fixtures\TestCases\RhubarbTestCase;
+use Rhubarb\Stem\Aggregates\Max;
+use Rhubarb\Stem\Tests\unit\Fixtures\TestContact;
+class MaxTest extends RhubarbTestCase
 {
+    protected function setUp()
+    {
+        parent::setUp();
 
+        $example = new TestContact();
+        $example->getRepository()->clearObjectCache();
+
+        $example = new TestContact();
+        $example->Forename = "a";
+        $example->CompanyID = 1;
+        $example->save();
+
+        $example = new TestContact();
+        $example->Forename = "b";
+        $example->CompanyID = 2;
+        $example->save();
+
+        $example = new TestContact();
+        $example->Forename = "c";
+        $example->CompanyID = 3;
+        $example->save();
+    }
+
+    public function testMax()
+    {
+        $examples = TestContact::find();
+
+        list($min) = $examples->calculateAggregates(
+            [new Max("CompanyID")]
+        );
+
+        $this->assertEquals(3, $min, "the maximum should have been 3, from 'c'");
+    }
 }
--- tests/unit/Aggregates/MinTest.php
+++ tests/unit/Aggregates/MinTest.php
@@ -1,15 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Luke
- * Date: 10/08/2016
- * Time: 09:30
- */
-
 namespace Rhubarb\Stem\Tests\unit\Aggregates;
 
-
-class MinTest
+use Rhubarb\Crown\Tests\Fixtures\TestCases\RhubarbTestCase;
+use Rhubarb\Stem\Aggregates\Min;
+use Rhubarb\Stem\Tests\unit\Fixtures\TestContact;
+class MinTest extends RhubarbTestCase
 {
+    protected function setUp()
+    {
+        parent::setUp();
 
+        $example = new TestContact();
+        $example->getRepository()->clearObjectCache();
+
+        $example = new TestContact();
+        $example->Forename = "a";
+        $example->CompanyID = 1;
+        $example->save();
+
+        $example = new TestContact();
+        $example->Forename = "b";
+        $example->CompanyID = 2;
+        $example->save();
+
+        $example = new TestContact();
+        $example->Forename = "c";
+        $example->CompanyID = 3;
+        $example->save();
+    }
+
+    public function testMin()
+    {
+        $examples = TestContact::find();
+
+        list($min) = $examples->calculateAggregates(
+            [new Min("CompanyID")]
+        );
+
+        $this->assertEquals(1, $min, "the minimum should have been 1, from 'a'");
+    }
 }
